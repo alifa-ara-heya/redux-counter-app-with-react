37a8e13c7664298d7046255b044657c3f52000c4
Alifa Ara Heya <alifaaraheya@gmail.com>
2025-06-27 16:58:01 -0400

    middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './features/counter/counterSlice'; //this was a default export, we can rename exports like this when importing
+import logger from './middlewares/logger';
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer
-    }
+    },
+    // middleware
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 });
 
 export type RootState = ReturnType<typeof store.getState>
